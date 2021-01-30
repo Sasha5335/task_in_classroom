d93f931fd0f5ee84d185dd40224391472cf47689
Aleksandr <aleksandr@gmail.com>
2021-01-30 15:16:56 +0200

    Add cards to the page via JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,114 +1,172 @@
 'use strict';
 
-const cardContainer = document.getElementById('root');
-const cardContainer = document.getElementById('person');
-const cards = responseData.map((place) => createPlaceCards(place));
+// const cardContainer = document.getElementById('card');
+// const cards = responseData.map((place) => createPlaceCards(place));
+// cardContainer.append(...cards);
 
-cardContainer.append(...cards);
 
-function createPlaceCards(place) {
-  const p = createElement(
-    'p',
-    { classNames: ['cardDescription'] },
-    document.createTextNode(place.description)
-  );
+// function createPlaceCards(place) {
+//    const h4 = createElement('h4', { classNames: ['card-name'] }, document.createTextNode(`${place.firstName} ${place.lastName}` || ''));
+//    const h5 = createElement('h5', { classNames: ['card-role'] }, document.createTextNode('☆' || ''));
+//    const p = createElement('p', { classNames: ['card-description'] }, document.createTextNode('Fusce dapibus, tellus ac cursus commodo, mauris condimentum nibh, utfermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo.'));
 
-  const h3 = createElement(
-    'h3',
-    { classNames: ['cardName'] },
-    document.createTextNode(place.name || '')
-  );
+//    const personContent = createElement(
+//       'div',
+//       { classNames: ['card-person-content'] },
+//       createImageWrapper(place),
+//       h4,
+//       h5,
+//       p
+//    );
 
-  const article = createElement(
-    'article',
-    { classNames: ['cardContainer'] },
-    createImageWrapper(place),
-    h3,
-    p
-  );
+//    return createElement('div', { classNames: ['card-person'] }, personContent);
+// }
 
-  return createElement('li', { classNames: ['cardWrapper'] }, article);
-}
 
-function createImageWrapper(place) {
-  const { name, id } = place;
+// function createImageWrapper(place) {
+//    const { firstName, id } = place;
 
-  const imageWrapper = document.createElement('div');
-  imageWrapper.setAttribute('id', `wrapper${id}`);
-  imageWrapper.classList.add('cardImageWrapper');
-  imageWrapper.style.backgroundColor = stringToColour(name);
+//    const imageWrapper = document.createElement('div');
+//    imageWrapper.setAttribute('id', `wrapper${id}`);
+//    imageWrapper.classList.add('card-img-wrapper');
+//    imageWrapper.style.backgroundColor = stringToColour(firstName);
 
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(name.trim().charAt(0) || ''));
+//    const initialsNamePerson = document.createElement('div');
+//    initialsNamePerson.classList.add('card-initials');
+//    initialsNamePerson.append(document.createTextNode(firstName.trim().charAt(0) || ''));
 
-  createImage(place, { className: 'cardImage' });
+//    createImage(place, { className: 'card-img' });
 
-  imageWrapper.append(initials);
-  return imageWrapper;
-}
+//    imageWrapper.append(initialsNamePerson);
+//    return imageWrapper;
+// }
 
-function createImage({ name, profilePicture, id }, { className }) {
-  const img = document.createElement('img');
-  img.classList.add(className);
-  img.dataset.id = id;
-  img.setAttribute('alt', name);
-  img.setAttribute('src', profilePicture);
-  img.addEventListener('error', handleImageError);
-  img.addEventListener('load', handleImageLoad);
-  return img;
-}
 
-/* 
-  EVENT HANDLERS
-*/
+// function createImage({ firstName, profilePicture, id }, { className }) {
+//    const img = document.createElement('img');
+//    img.classList.add(className);
+//    img.dataset.id = id;
+//    img.setAttribute('alt', firstName);
+//    img.setAttribute('src', profilePicture);
+//    img.addEventListener('error', handleImageError);
+//    img.addEventListener('load', handleImageLoad);
+//    return img;
+// }
 
-function handleImageError({ target }) {
-  target.remove();
-}
 
-function handleImageLoad({
-  target,
-  target: {
-    dataset: { id },
-  },
-}) {
-  document.getElementById(`wrapper${id}`).append(target);
-}
+// /* 
+//   EVENT HANDLERS
+// */
+// function handleImageError({ target }) {
+//    target.remove();
+// }
 
-/* 
-  UTILS
-*/
+// function handleImageLoad({
+//    target,
+//    target: {
+//       dataset: { id },
+//    },
+// }) {
+//    document.getElementById(`wrapper${id}`).append(target);
+// }
 
-function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    let value = (hash >> (i * 8)) & 0xff;
-    colour += ('00' + value.toString(16)).substr(-2);
-  }
-  return colour;
-}
 
-/* 
-  LIB
-*/
-/**
- *
- * @param {string} type
- * @param {object} options
- * @param {string[]} options.classNames - css classes
- * @param {function} options.onClick - click handler
- * @param {Node[]} children
- * @return {HTMLElement}
- */
-function createElement(type, { classNames, onClick }, ...children) {
-  const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.onclick = onClick;
-  elem.append(...children);
-  return elem;
-}
+// // /* 
+// //   UTILS
+// // */
+// function stringToColour(str) {
+//    let hash = 0;
+//    for (let i = 0; i < str.length; i++) {
+//       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+//    }
+//    let colour = '#';
+//    for (let i = 0; i < 3; i++) {
+//       let value = (hash >> (i * 8)) & 0xff;
+//       colour += ('00' + value.toString(16)).substr(-2);
+//    }
+//    return colour;
+// }
+
+
+// /* 
+//   LIB
+// */
+// /**
+//  *
+//  * @param {string} type
+//  * @param {object} options
+//  * @param {string[]} options.classNames - css classes
+//  * @param {function} options.onClick - click handler
+//  * @param {Node[]} children
+//  * @return {HTMLElement}
+//  */
+// function createElement(type, { classNames, onClick }, ...children) {
+//    const elem = document.createElement(type);
+//    elem.classList.add(...classNames);
+//    elem.onclick = onClick;
+//    elem.append(...children);
+//    return elem;
+// }
+
+
+
+
+
+
+
+// const cardContainer = document.getElementById('root');
+// const cards = data.map((place) => createPlaceCards(place));
+// cardContainer.append(...cards);
+
+// function createPlaceCards(place) {
+//    const p = createElement(
+//       'p',
+//       { classNames: ['cardDescription'] },
+//       document.createTextNode(place.description)
+//    );
+
+//    const h3 = createElement(
+//       'h3',
+//       { classNames: ['cardName'] },
+//       document.createTextNode(place.name || '')
+//    );
+
+//    const article = createElement(
+//       'article',
+//       { classNames: ['cardContainer'] },
+//       createImageWrapper(place),
+//       h3,
+//       p
+//    );
+
+//    return createElement('li', { classNames: ['cardWrapper'] }, article);
+// }
+
+// function createImageWrapper(place) {
+//    const { name, id } = place;
+
+//    const imageWrapper = document.createElement('div');
+//    imageWrapper.setAttribute('id', `wrapper${ id }`);
+//    imageWrapper.classList.add('cardImageWrapper');
+//    imageWrapper.style.backgroundColor = stringToColour(name);
+
+//    const initials = document.createElement('div');
+//    initials.classList.add('initials');
+//    initials.append(document.createTextNode(name.trim().charAt(0) || ''));
+
+//    createImage(place, { className: 'cardImage' });
+
+//    imageWrapper.append(initials);
+//    return imageWrapper;
+// }
+
+// function createImage({ name, profilePicture, id }, { className }) {
+//    const img = document.createElement('img');
+//    img.classList.add(className);
+//    img.dataset.id = id;
+//    img.setAttribute('alt', name);
+//    img.setAttribute('src', profilePicture);
+//    img.addEventListener('error', handleImageError);
+//    img.addEventListener('load', handleImageLoad);
+//    return img;
+// }
--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,5 +1,5 @@
-new URL('https://www.facebook.com/JasonStatham/');
-new Map()
-  .set('www.facebook.com', 'src_to_icon')
-  .set('www.facebook.com', 'src_to_icon')
-  .set('www.facebook.com', 'src_to_icon');
+// new URL('https://www.facebook.com/JasonStatham/');
+// new Map()
+//   .set('www.facebook.com', 'src_to_icon')
+//   .set('www.facebook.com', 'src_to_icon')
+//   .set('www.facebook.com', 'src_to_icon');
